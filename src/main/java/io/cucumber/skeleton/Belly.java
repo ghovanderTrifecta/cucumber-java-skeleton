c050d12468f5275e5c9996acbacc8cfafd25f653
Garrett Hovander <garrett.hovander@trifectanutrition.com>
2019-04-03 15:24:32 -0700

    Created Successful Gherkin Scenario

--- src/main/java/io/cucumber/skeleton/Belly.java
+++ src/main/java/io/cucumber/skeleton/Belly.java
@@ -1,7 +1,19 @@
 package io.cucumber.skeleton;
 
-public class Belly {
-    public void eat(int cukes) {
+import static org.junit.Assert.*;
 
+public class Belly {
+
+    public void eat(int cukes) {
+        System.out.println(String.format("Eating %s cucumbers!", cukes));
     }
+
+    public void wait(int hours){
+        System.out.println(String.format("Waiting %s hours!", hours));
+    }
+
+    public void growl(boolean isGrowling){
+        assertEquals(true, isGrowling);
+    }
+
 }
--- src/test/java/io/cucumber/skeleton/Stepdefs.java
+++ src/test/java/io/cucumber/skeleton/Stepdefs.java
@@ -1,11 +1,27 @@
 package io.cucumber.skeleton;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class Stepdefs {
+    private Belly belly = null;
+
     @Given("^I have (\\d+) cukes in my belly$")
-    public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
-        Belly belly = new Belly();
+    public void I_have_cukes_in_my_belly(int cukes) {
+        belly = new Belly();
         belly.eat(cukes);
+    }
+
+    @When("^I wait (\\d+) hour")
+    public void I_wait_hours(int hours) throws Throwable {
+        if(belly != null) belly.wait(hours);
+        else throw new Exception("Belly object does not exist");
+    }
+
+    @Then("my belly should growl")
+    public void My_belly_should_growl() throws Throwable {
+        if(belly != null) belly.growl(true);
+        else throw new Exception("Belly object does not exist");
     }
 }
