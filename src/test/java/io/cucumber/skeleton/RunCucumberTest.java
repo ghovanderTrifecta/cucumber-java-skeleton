95b167df2fd53b044e4eda14e9817fa560881a4f
Garrett Hovander <garrett.hovander@trifectanutrition.com>
2019-04-03 17:40:28 -0700

    Added Reporting
    Test Execution filters by tags and feature directory locations. Added curl command for posting to HipTest.

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -3,8 +3,15 @@ package io.cucumber.skeleton;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.RunnerBuilder;
+
+import static org.junit.runners.Suite.*;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
-public class RunCucumberTest {
+@CucumberOptions(tags="@smoke", features="out/test/resources/io/cucumber/skeleton", plugin = {"pretty", "junit:out/reports/cucumber.xml","json:out/reports/Cucumber.json"})
+public class RunCucumberTest{
 }
+
+
--- src/test/resources/io/cucumber/skeleton/belly.feature
+++ src/test/resources/io/cucumber/skeleton/belly.feature
@@ -1,6 +1,19 @@
 Feature: Belly
 
+  @smoke
   Scenario: a few cukes
     Given I have 42 cukes in my belly
+    When I wait 1 hour
+    Then my belly should growl
+
+  @smoke
+  Scenario: a few more cukes
+    Given I have 60 cukes in my belly
+    When I wait 1 hour
+    Then my belly should growl
+
+  @smoke
+  Scenario: a lot of cukes
+    Given I have 80 cukes in my belly
     When I wait 1 hour
     Then my belly should growl
